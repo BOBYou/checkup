c301be9e78344650739eec1f9bd07a0a0f7084d1
hel2o <601902498@qq.com>
2017-08-28 09:02:40 +0800

    更新bug 全局计数器不置0的情况

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
-
 import (
-	"time"
-	"runtime"
+	"flag"
+	"fmt"
 	"log"
 	"os"
-  "strings"
-	"fmt"
-	"flag"
-  "strconv"
 	"os/exec"
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
+
 	"github.com/gaochao1/sw"
 	"github.com/hel2o/checkup/g"
 )
@@ -19,7 +19,6 @@ type Ping struct {
 	Ip  string
 	Rtt float64
 }
-
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
@@ -40,7 +39,7 @@ func main() {
 	var HostName string
 	if g.Config().Checkup.HostName == "" {
 		HostName, _ = os.Hostname()
-	}else{
+	} else {
 		HostName = g.Config().Checkup.HostName
 	}
 
@@ -53,7 +52,7 @@ func main() {
 func Interval(ips []string, hostName string, failsInterval *int) {
 	t1 := time.Now()
 	fails := 0
-  iptables := CheckIptable()
+	iptables := CheckIptable()
 
 	chs := make([]chan Ping, len(ips))
 
@@ -64,51 +63,52 @@ func Interval(ips []string, hostName string, failsInterval *int) {
 
 	for _, ch := range chs {
 		t2 := time.Now()
-		Result := <- ch
-		if Result.Rtt == -1{
+		Result := <-ch
+		if Result.Rtt == -1 {
 			fails++
 		}
 		log.Println("IP:", Result.Ip, "	Rtt:", Result.Rtt, "	Cost:", time.Since(t2))
 	}
-  f := float64(fails) / float64(len(ips))
-  log.Println("iptables: ",iptables)
+	f := float64(fails) / float64(len(ips))
+	log.Println("iptables: ", iptables)
 
-	if f >= g.Config().Checkup.FailureRate{
+	if f >= g.Config().Checkup.FailureRate {
 		*failsInterval++
-		log.Println("FailsInterval:",*failsInterval)
-		if *failsInterval >= g.Config().Checkup.FailsInterval  && iptables == false{
-						Iptables("sh/input")
-				    urlData_input := make(map[string][]string, 2)
-				    content_input := hostName + "\nPing次数：" + strconv.Itoa(len(ips)) + "\n失败次数：" + strconv.Itoa(fails) + "\n执行操作：Add Reject\n" + "时间：" + time.Now().Format("2006-01-02 15:04:05") + "\n以上内容通过SendWeChat_Api发送"
-				    urlData_input["content"] = []string{content_input}
-				    urlData_input["to"] = []string{g.Config().Checkup.To}
+		log.Println("FailsInterval:", *failsInterval)
+		if *failsInterval >= g.Config().Checkup.FailsInterval && iptables == false {
+			Iptables("sh/input")
+			urlData_input := make(map[string][]string, 2)
+			content_input := hostName + "\nPing次数：" + strconv.Itoa(len(ips)) + "\n失败次数：" + strconv.Itoa(fails) + "\n执行操作：Add Reject\n" + "时间：" + time.Now().Format("2006-01-02 15:04:05") + "\n以上内容通过SendWeChat_Api发送"
+			urlData_input["content"] = []string{content_input}
+			urlData_input["to"] = []string{g.Config().Checkup.To}
 
-				    _, err := g.Post(g.Config().Checkup.PostUrl,urlData_input)
-				    if err != nil {
-				      log.Println("error: %v", err)
-				    }
-				    log.Println("intput")
-				}
-	}else if f < g.Config().Checkup.FailureRate  && iptables == true{
+			_, err := g.Post(g.Config().Checkup.PostUrl, urlData_input)
+			if err != nil {
+				log.Println("error: %v", err)
+			}
+			log.Println("intput")
+		}
+	} else if f < g.Config().Checkup.FailureRate && iptables == true {
 
 		Iptables("sh/remove")
 		*failsInterval = 0
-    urlData_remove := make(map[string][]string, 2)
-    content_remove := hostName + "\nPing次数：" + strconv.Itoa(len(ips)) + "\n失败次数：" + strconv.Itoa(fails) + "\n执行操作：Remove Reject\n" + "时间：" + time.Now().Format("2006-01-02 15:04:05") + "\n以上内容通过SendWeChat_Api发送"
-    urlData_remove["content"] = []string{content_remove}
-    urlData_remove["to"] = []string{g.Config().Checkup.To}
+		urlData_remove := make(map[string][]string, 2)
+		content_remove := hostName + "\nPing次数：" + strconv.Itoa(len(ips)) + "\n失败次数：" + strconv.Itoa(fails) + "\n执行操作：Remove Reject\n" + "时间：" + time.Now().Format("2006-01-02 15:04:05") + "\n以上内容通过SendWeChat_Api发送"
+		urlData_remove["content"] = []string{content_remove}
+		urlData_remove["to"] = []string{g.Config().Checkup.To}
 
-    _, err := g.Post(g.Config().Checkup.PostUrl,urlData_remove)
-    if err != nil {
-      log.Println("error: %v", err)
-    }
-    log.Println("remove")
-	}else {
-    log.Println("nothing to do!")
-  }
+		_, err := g.Post(g.Config().Checkup.PostUrl, urlData_remove)
+		if err != nil {
+			log.Println("error: %v", err)
+		}
+		log.Println("remove")
+	} else {
+		*failsInterval = 0
+		log.Println("nothing to do!")
+	}
 
 	elapsed := time.Since(t1)
-	log.Println("Runing time: ", elapsed," Fails:", fails, " FailsInterval:",*failsInterval, "\n")
+	log.Println("Runing time: ", elapsed, " Fails:", fails, " FailsInterval:", *failsInterval, "\n")
 }
 
 func GoPing(ch chan Ping, ip string) {
@@ -145,17 +145,17 @@ func AllCheckupIp() (allIp []string) {
 }
 
 func Iptables(run string) {
-	cmd := exec.Command("/bin/sh",run)
+	cmd := exec.Command("/bin/sh", run)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		log.Println("error",err)
+		log.Println("error", err)
 	}
 	log.Println(string(out))
 }
 
-func CheckIptable() bool{
-  cmd := exec.Command("iptables","-L", "-n")
-  out, _ := cmd.CombinedOutput()
-  check := strings.Contains(string(out),"reject keepalived connect")
-  return check
+func CheckIptable() bool {
+	cmd := exec.Command("iptables", "-L", "-n")
+	out, _ := cmd.CombinedOutput()
+	check := strings.Contains(string(out), "reject keepalived connect")
+	return check
 }
